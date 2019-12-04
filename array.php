c554061ac25e23c15012df6e2518b4c1bd4b53d1
shinder <lsd0125@gmail.com>
2019-12-04 15:46:45 +0800

    update template.php

--- array.php
+++ array.php
@@ -6,12 +6,34 @@
 //     echo $arr[$i]."<br />";
 // }
 
-$strTags = "glasses,hens,women,ccc";
-$arrTags = explode(',', $strTags);
+// $strTags = "glasses,hens,women,ccc";
+// $arrTags = explode(',', $strTags);
+// print_r($arrTags);
+
+$arr = [
+    [
+        "productId" => 1,
+        "productName" => "水壺",
+        "productPrice" => 150
+    ],
+    [
+        "productId" => 2,
+        "productName" => "畫冊",
+        "productPrice" => 350
+    ],
+    [
+        "productId" => 3,
+        "productName" => "原子筆",
+        "productPrice" => 25
+    ],
+];
+
+for($i = 0; $i < count($arr); $i++){
+    echo "ProductId = {$arr[$i]['productId']} ";
+    echo "ProductName = {$arr[$i]['productName']} ";
+    echo "ProductPrice = {$arr[$i]['productPrice']} ";
+    echo "<br />";
+}
+
 ?>
 
-<pre>
-<?php
-print_r($arrTags);
-?>
-</pre>
